fea577a8dc1da407db968b96d9c6af49c440f3d2
sjtuLLWWTT <100546566+sjtuLLWWTT@users.noreply.github.com>
2022-05-11 16:53:48 +0800

    Delete ws&nodejs-chatroom directory

--- ws&nodejs-chatroom/src/common/util.js
+++
@@ -1,6 +0,0 @@
-export const isScrollBottom = (ele, wrapHeight, threshold) => {
-  const h1 = ele.scrollHeight - ele.scrollTop
-  const h2 = wrapHeight + threshold
-  const isBottom = h1 <= h2
-  return isBottom
-}
